26bc1b168984e10c4f44ec6a9a6515a17b28d7ed
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2023-06-19 16:29:04 +0900

    2023.06.19

--- webd/webd_test/A-3/script/script.js
+++ webd/webd_test/A-3/script/script.js
@@ -1,18 +1,18 @@
-$(function(){
+$(function () {
     // 메뉴 : 하나씩
-    $(".nav > ul > li").mouseover(function(){
+    $(".nav > ul > li").mouseover(function () {
         $(this).find(".submenu").stop().slideDown();
     });
-    $(".nav > ul > li").mouseout(function(){
+    $(".nav > ul > li").mouseout(function () {
         $(this).find(".submenu").stop().slideUp();
     });
 
     // 슬라이드 : 페이드 효과
     let currentIndex = 0;
     const $slider = $(".slider .slider");
-    $slider.hide().first().show();  //모든 이미지 숨기고 첫 번째 이미지 나타남
+    $slider.hide().first().show(); //모든 이미지 숨기고 첫 번째 이미지 나타남
 
-    setInterval(function(){
+    setInterval(function () {
         let nextIndex = (currentIndex + 1) % $slider.length;
 
         $slider.eq(currentIndex).fadeOut(1200);
@@ -22,12 +22,12 @@ $(function(){
     }, 3000);
 
     // 탭 메뉴
-    const tabBtn = $(".info-menu > a");
-    const tabCont = $(".info-cont > ul");
+    let tabBtn = $(".info-menu > a");
+    let tabCont = $(".info-cont > ul");
 
     tabCont.hide().eq(0).show();
 
-    tabBtn.on("click", function(){
+    tabBtn.click(function () {
         const index = $(this).index();
 
         $(this).addClass("active").siblings().removeClass("active");
